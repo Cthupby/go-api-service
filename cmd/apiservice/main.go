4f600f228c732f3a460ec5001ee2ecedb2f1b8d0
Cthupby <Cthupby@gmail.com>
2024-06-25 20:47:01 +0300

    Update main.go

--- cmd/apiservice/main.go
+++ cmd/apiservice/main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"go-api-service/internal/controllers"
 )
 
 func main() {
-	fmt.Println("Start server!")
+	controller := controllers.New()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", controller.HandleRequests)
+
+	server := &http.Server{
+		Addr: fmt.Sprintf("localhost:5000"),
+		Handler: mux,
+	}
+
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		panic(err)
+	}
 }
